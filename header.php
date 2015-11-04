cc38de52db576535782d4b1cf037794cc03bd348
Larry Kagan <larry@superiocity.com>
2015-11-04 10:04:08 -0500

    Building front page.

--- header.php
+++ header.php
@@ -16,26 +16,19 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'superiocity_rchn' ); ?></a>
-
+<div id="page" class="hfeed">
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class="logo" src="<?= esc_url( get_stylesheet_directory_uri() )?>/images/rc-heli-nation-logo.svg" alt="<?php bloginfo( 'name' ); ?>"></a>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'superiocity_rchn' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">menu <i class="fa fa-bars"></i></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -12,10 +12,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php superiocity_rchn_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -38,6 +36,5 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php superiocity_rchn_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
