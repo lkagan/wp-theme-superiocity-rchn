2f8c9227546202af0ee2055c2211b7e07d10942b
Larry Kagan <larry@superiocity.com>
2016-02-02 15:28:13 -0500

    Minor tweaks.

--- 404.php
+++ 404.php
@@ -18,7 +18,7 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'Nothing can be found at this address. Try the menu or a search.', 'superiocity_rchn' ); ?></p>
+					<p>Did you <em>dumb thumb</em> the web address? Try the menu or a search.</p>
 					<br>
 					<img src="<?php echo get_template_directory_uri(); ?>/images/justin-crash.jpg" class="image-card">
 				</div><!-- .page-content -->
